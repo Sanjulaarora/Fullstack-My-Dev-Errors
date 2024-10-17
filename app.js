cd74061ae5c8aa7f3d8968d7fb467d886cf96e17
Sanjulaarora <sanjulaarora3005@gmail.com>
2024-10-17 07:34:19 +0530

    fixed bug

--- app.js
+++ app.js
@@ -6,18 +6,13 @@ require("./db/conn");
 const Blogs = require("./models/blogSchema");
 const cors = require("cors");
 const router = require("./routes/router");
-const path = require("path");
 
 app.use(express.json());
 app.use(cors());
 app.use(router);
 
-const port = process.env.PORT || 8003;
-
-//-------------------Deployment----------------------//
-    app.use(express.static(path.join(__dirname, "client/build")));
-//-------------------Deployment----------------------//
+const port = 8003;
 
 app.listen(port,() => {
     console.log(`server is started at port number ${port}`);
-})
+});
